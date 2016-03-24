56a43596a3703c5631c242253bc5949c508afe27
Joshua M <jjmalavolti@gmail.com>
2016-03-24 15:08:43 -0500

    Update setup.sh
    Added comments

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
-#
+# Author: Malamaker (J.Malavolti) Created: 2016-03-24 Last Updated: 2016-03-24
 #
 
+# Chage to App Folder and copies desktop icon to the Desktop directory
 cd ~/UC-Davis-AR-Sandbox-Configuration-Menu/
 cp ./ar-sandbox.desktop ~/Desktop/
 
+# Replaces the HOME token with the HOME environmental variable
 sudo sed -i "s|%%HOME%%|$HOME|g" ~/Desktop/ar-sandbox.desktop
